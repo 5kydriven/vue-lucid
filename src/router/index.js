408264ca493cce7045530611eb811cb621627a41
5kydriven <tagpuno619@gmail.com>
2024-01-03 23:48:14 +0800

    added some new path

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '../pages/Homepage.vue'
+import Authpage from '../pages/AuthPage.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'homepage',
     component: Homepage
-  }
+  },
+  {
+    path: '/signup',
+    component: Authpage
+  },
+  {
+    path: '/signin',
+    component: Authpage
+  },
+  
 ]
 
 const router = createRouter({
